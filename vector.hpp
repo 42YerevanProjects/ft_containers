4469a47610591dbae0489440ac910a0a1edd75a9
Sargis-Hovsepyan <sargishovsepyan2001@gmail.com>
2022-06-22 02:20:09 +0400

    Added typedefs

--- vector.hpp
+++ vector.hpp
@@ -7,12 +7,18 @@ namespace ft
     class vector 
     {
         public:
-            typedef T               value_type;
-            typedef Alloc           alloc_type;
-            typedef T&              reference;
-            typedef const T&        const_reference;
-            typedef T*              pointer;
-            typedef const T*        const_pointer;
+            typedef T                       value_type;
+            typedef Alloc                   alloc_type;
+            typedef T&                      reference;
+            typedef const T&                const_reference;
+            typedef T*                      pointer;
+            typedef const T*                const_pointer;
+            
+            /* To Be implemented... */
+            typedef RandomIt<T, T*, T&>                 iterator;
+            typedef RandomIt<T, const T*, const T&>     const_iterator;
+            typedef RevRandomIt<T, T*, T&>              reverse_iterator;
+            typedef RevRandomIt<T, const T*, const T&>  const_reverse_iterator;
         
     };
 }
