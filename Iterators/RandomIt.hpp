b9e646a8355a407c7222fae85be16931257fd040
Sargis-Hovsepyan <sargishovsepyan2001@gmail.com>
2022-06-25 23:49:54 +0400

    Implemented the interface of a random access iterator

--- Iterators/RandomIt.hpp
+++ Iterators/RandomIt.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <cstdlib>
+
 /*
 ==============================
     Random Access Iterator
@@ -13,6 +15,39 @@ class RandomIt
     public:
         typedef RandomIt<T, Pointer, Reference> RandIter;
         typedef RandomIt<T, T*, T&>             iterator;
-   
-        /* To be continued... */
+
+        Pointer data_ptr;
+
+        /* Constructors and Destructor */
+
+        RandomIt();
+        RandomIt(const Pointer ptr);
+        RandomIt(const iterator& other);
+        ~RandomIt();
+
+        /* Operator Overloads */
+        
+        RandIter&   operator=(const iterator& other);
+
+        RandIter&   operator++(int);
+        RandIter&   operator++();
+
+        RandIter&   operator+(size_t n) const;
+        RandIter&   operator+=(size_t n);
+
+        RandIter&   operator--(int);
+        RandIter&   operator--();
+
+        RandIter&   operator-(size_t n) const;
+        size_t      operator-(RandIter it) const;
+
+        RandIter&   operator-=(size_t n);
+
+        Reference   operator*();
+        Pointer     operator->();
+        Reference   operator[](size_t n);
+
+        /* Equality Checks */
+        bool operator==(const RandIter &other) const;
+        bool operator!=(const RandIter &other) const;
 };
