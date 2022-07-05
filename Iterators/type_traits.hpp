a536e59f37a58fdeec2bca69c57d9fa81ef90966
Sargis-Hovsepyan <sargishovsepyan2001@gmail.com>
2022-07-06 02:48:44 +0400

    Added the is_integral structs

---
+++ Iterators/type_traits.hpp
@@ -0,0 +1,145 @@
+#pragma once
+
+#include <cstddef>
+
+namespace ft
+{
+
+    /*
+    ==============================
+        Iterator Category Tags
+    ==============================
+    */
+
+    struct input_iterator_tag {};
+    struct output_iterator_tag {};
+    struct forward_iterator_tag : public input_iterator_tag {};
+    struct bidirectional_iterator_tag : public forward_iterator_tag {};
+    struct random_access_iterator_tag : public bidirectional_iterator_tag {};
+
+
+    /*
+    =======================
+        Iterator Traits
+    =======================
+    */
+    
+    template <class It>
+	struct	iterator_traits
+	{
+		typedef typename It::value_type         value_type;
+		typedef typename It::pointer            pointer;
+		typedef typename It::reference          reference;
+		typedef typename It::difference_type    difference_type;
+		typedef typename It::iterator_category  iterator_category;
+	};
+
+	template <class T>
+	struct	iterator_traits<T*>
+	{
+		typedef T                                   value_type;
+		typedef T*                                  pointer;
+		typedef T&                                  reference;
+		typedef std::ptrdiff_t                      difference_type;
+		typedef ft::random_access_iterator_tag      iterator_category;
+	};
+
+	template <class T>
+	struct	iterator_traits<const T*>
+	{
+		typedef T                                   value_type;
+		typedef const T*                            pointer;
+		typedef const T&                            reference;
+		typedef std::ptrdiff_t                      difference_type;
+		typedef ft::random_access_iterator_tag      iterator_category;
+	};
+
+
+    /*
+    =================
+        Enable If
+    =================
+    */
+
+    template <bool B, class T = void>
+	struct enable_if {};
+
+	template <class T>
+	struct enable_if<true, T> { typedef T type; };
+
+
+    /*
+    ===================
+        Conditional
+    ===================
+    */
+
+    template <bool B, class T = void, class U = void>
+	struct conditional {};
+
+	template <class T, class U>
+	struct conditional<false, T, U> { typedef T type; };
+
+	template <class T, class U>
+	struct conditional<true, T, U> { typedef U type; };
+
+
+
+    /*
+    ======================
+        Integral types
+    ======================
+    */
+
+    template <bool is_integral, typename T>
+    struct is_integral_res 
+    { 
+        typedef T type; 
+        static const bool value = is_integral; 
+    };
+
+
+    template <typename>
+    struct is_integral_type : public is_integral_res<false, bool> {};
+
+    template <>
+    struct is_integral_type<bool> : public is_integral_res<true, bool> {};
+
+    template <>
+    struct is_integral_type<char> : public is_integral_res<true, char> {};
+
+    template <>
+    struct is_integral_type<signed char> : public is_integral_res<true, signed char> {};
+
+    template <>
+    struct is_integral_type<short int> : public is_integral_res<true, short int> {};
+
+    template <>
+    struct is_integral_type<int> : public is_integral_res<true, int> {};
+
+    template <>
+    struct is_integral_type<long int> : public is_integral_res<true, long int> {};
+
+    template <>
+    struct is_integral_type<long long int> : public is_integral_res<true, long long int> {};
+
+    template <>
+    struct is_integral_type<unsigned char> : public is_integral_res<true, unsigned char> {};
+
+    template <>
+    struct is_integral_type<unsigned short int> : public is_integral_res<true, unsigned short int> {};
+
+    template <>
+    struct is_integral_type<unsigned int> : public is_integral_res<true, unsigned int> {};
+
+    template <>
+    struct is_integral_type<unsigned long int> : public is_integral_res<true, unsigned long int> {};
+
+    template <>
+    struct is_integral_type<unsigned long long int> : public is_integral_res<true, unsigned long long int> {};
+
+
+    template <typename T>
+    struct is_integral : public is_integral_type<T> {};
+
+}
