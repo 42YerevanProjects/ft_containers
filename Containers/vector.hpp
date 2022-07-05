3967935a8ad1ece4ad3fb2188be6ec29962544ad
Sargis-Hovsepyan <sargishovsepyan2001@gmail.com>
2022-07-06 02:44:15 +0400

    changed the iterator clasees, made the destructor virtual and member variables protected

--- Containers/vector.hpp
+++ Containers/vector.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "../Iterators/Iterator.hpp"
+#include "../Iterators/iterator.hpp"
 #include <memory>
 
 namespace ft 
@@ -19,29 +19,30 @@ namespace ft
             typedef size_t                  size_type;
             
             /* Iterators of Vector */
-            typedef RandomIt<T>             iterator;
-            typedef RandomIt<T, true>       const_iterator;
-            typedef RevRandomIt<T>          reverse_iterator;
-            typedef RevRandomIt<T, true>    const_reverse_iterator;
+            typedef ft::random_iterator<T>                  iterator;
+            typedef ft::random_iterator<const T>            const_iterator;
+            typedef ft::reverse_iterator<iterator>          reverse_iterator;
+            typedef ft::reverse_iterator<const_iterator>    const_reverse_iterator;
 
-        private:
-            allocator_type  alloc;
-            pointer         data;
-            size_type       len;
-            size_type       cap;
+        protected:
+            allocator_type  _alloc;
+            pointer         _data;
+            size_type       _size;
+            size_type       _cap;
 
         public:
             /* Constructers and Destructer */
             explicit vector(const allocator_type& alloc = allocator_type());
             explicit vector (size_t n, const value_type& val = value_type(), const allocator_type& alloc = allocator_type());
 
-            vector(iterator first, iterator last, const allocator_type& alloc = allocator_type());
-            vector(const_iterator first, const_iterator last, const allocator_type& alloc = allocator_type());
+            template<typename InputIterator>
+            vector(InputIterator first, InputIterator last, const allocator_type& alloc = allocator_type(),
+                    typename ft::enable_if<!ft::is_integral<InputIterator>::value, InputIterator>::type* = 0);
 
             vector(const vector& other);
             vector& operator=(const vector& other);
 
-            ~vector();
+            virtual ~vector();
 
             /* Iterator Functions */
             iterator                begin();
@@ -75,8 +76,10 @@ namespace ft
             const_reference         back() const;
 
             /* Modifier Functions */
-            void                    assign(iterator first, iterator last);
-            void                    assign(const_iterator first, const_iterator last);
+            template<typename InputIterator>
+            void                    assign(InputIterator first, InputIterator last,
+            typename ft::enable_if<!ft::is_integral<InputIterator>::value, InputIterator>::type* = 0);
+
             void                    assign(size_type n, const value_type& val);
             void                    push_back(const value_type& val);
             void                    pop_back();
