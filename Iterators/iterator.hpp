9f8a8927e3e0953b74f80c8822641995711212ff
Sargis-Hovsepyan <sargishovsepyan2001@gmail.com>
2022-07-08 23:59:37 +0400

    Added iterator tag check structs

--- Iterators/iterator.hpp
+++ Iterators/iterator.hpp
@@ -20,4 +20,43 @@ namespace ft
         typedef Distance    difference_type;
         typedef Category    iterator_category;
     };
+
+
+    /*
+    ==============================
+        Iterator Tag Checks
+    ==============================
+    */
+
+    template<bool is_valid, class T>
+    struct valid_iterator_tag_res
+    {
+        typedef T   type;
+        static const bool value = is_valid;
+    };
+
+    template<class T>
+    struct is_iterator_tagged : valid_iterator_tag_res<false, T> {};
+
+    template<>
+    struct is_iterator_tagged<ft::random_access_iterator_tag> 
+        : valid_iterator_tag_res<true, ft::random_access_iterator_tag> {};
+
+    template<>
+    struct is_iterator_tagged<ft::bidirectional_iterator_tag> 
+        : valid_iterator_tag_res<true, ft::bidirectional_iterator_tag> {};
+
+    template<>
+    struct is_iterator_tagged<ft::forward_iterator_tag> 
+        : valid_iterator_tag_res<true, ft::forward_iterator_tag> {};
+
+    template<>
+    struct is_iterator_tagged<ft::input_iterator_tag> 
+        : valid_iterator_tag_res<true, ft::input_iterator_tag> {};
+
+    template<>
+    struct is_iterator_tagged<ft::output_iterator_tag> 
+        : valid_iterator_tag_res<true, ft::output_iterator_tag> {};
 }
+
+
