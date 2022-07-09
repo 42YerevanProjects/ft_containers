dadfcfb15577af2f23e331abd3bbbd061e9e9419
Sargis-Hovsepyan <sargishovsepyan2001@gmail.com>
2022-07-09 15:06:44 +0400

    Fixed a typo

--- Iterators/reverse_iterator.hpp
+++ Iterators/reverse_iterator.hpp
@@ -14,7 +14,7 @@ namespace ft
     class reverse_iterator
     {
         public:
-            typedef typename ft::iterator_traits<It>::type              value_type;
+            typedef typename ft::iterator_traits<It>::value_type        value_type;
             typedef typename ft::iterator_traits<It>::pointer           pointer;
             typedef typename ft::iterator_traits<It>::reference         reference;
             typedef typename ft::iterator_traits<It>::difference_type   difference_type;
