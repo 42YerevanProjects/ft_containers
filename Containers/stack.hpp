2745c5c4284f155346d4eeff8fc6117b9b4c40fb
Sargis-Hovsepyan <sargishovsepyan2001@gmail.com>
2022-07-13 00:19:23 +0400

    stack template class interface

---
+++ Containers/stack.hpp
@@ -0,0 +1,49 @@
+#pragma once
+
+#include "vector.hpp"
+
+namespace ft
+{
+    template<class T, class Container = ft::vector<T> >
+    class stack
+    {
+        public:
+            typedef T           value_type;
+            typedef size_t      size_type;
+            typedef Container   container_type;
+
+        protected:
+            container_type  _ctnr;
+
+        public:
+                /* Constructor and Destructor */
+
+                explicit stack(const container_type& ctnr = container_type());
+                ~stack();
+
+                /* Capacity Functions */
+
+                size_type           size() const;
+                bool                empty() const;
+
+                /* Element Access Functions */
+
+                value_type&         top();
+                const value_type&   top() const;
+
+                /* Element Modifier Functions */
+
+                void                push(const value_type& val);
+                void                pop();
+                void                swap(stack& x);
+
+                /* Non-Member Function */
+                
+                template<class U, class Cont>
+                friend bool         operator==(const stack<U, Cont>&, const stack<U, Cont>&);
+
+                template<class U, class Cont>
+                friend bool         operator<(const stack<U, Cont>&, const stack<U, Cont>&);
+    };
+}
+#include "stack.tpp"
