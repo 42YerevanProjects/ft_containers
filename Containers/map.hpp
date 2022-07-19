dd383c9dccac6fe2ce3bf1c29ea0b9ce18235334
Sargis-Hovsepyan <sargishovsepyan2001@gmail.com>
2022-07-19 08:35:54 +0400

    Initial commit

---
+++ Containers/map.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "../Utility/rb_tree_node.hpp"
+
+namespace ft
+{
+    template < class Key,
+               class T
+               //class Compare = less<Key>,
+               //class Alloc = std::allocator<pair<const key, T> >
+             >
+    class map
+    {
+        
+    };
+}
