f16b3b24943491bc0bfdfad3221c99b06d132e0e
Sargis Hovsepyan <60169060+Sargis-Hovsepyan@users.noreply.github.com>
2022-07-30 18:09:40 +0400

    Update rb_tree.hpp

--- Utility/rb_tree.hpp
+++ Utility/rb_tree.hpp
@@ -76,8 +76,8 @@ namespace ft
 
             /* Modifier Functions */
 
-            iterator                insert(const_iterator positin, const value_type& val);
-            iterator                insert(iterator positin, const value_type& val);
+            iterator                insert(const_iterator position, const value_type& val);
+            iterator                insert(iterator position, const value_type& val);
             
             template <typename InputIterator>
             void                    insert(InputIterator first, InputIterator last);
