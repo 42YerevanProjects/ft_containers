6f84afde1d92f1ad9402b7039bb5531da9b091cf
Sargis-Hovsepyan <sargishovsepyan2001@gmail.com>
2022-08-02 23:51:48 +0400

    Include header that containes iterator includes

---
+++ Includes/iterators.hpp
@@ -0,0 +1,8 @@
+#pragma oonce
+
+#include "../Iterators/iterator.hpp"
+#include "../Iterators/type_traits.hpp"
+#include "../Iterators/List/random_iterator.hpp"
+#include "../Iterators/List/reverse_iterator.hpp"
+#include "../Iterators/Tree/rb_tree_iterator.hpp"
+#include "../Iterators/Tree/const_rb_tree_iterator.hpp"
