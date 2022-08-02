3a3c01cd77c5c3227738077d3c7cc9f0ed652d07
Sargis-Hovsepyan <sargishovsepyan2001@gmail.com>
2022-08-03 00:17:51 +0400

    Fixed teh template parameters by using ft::pair

--- Containers/map.hpp
+++ Containers/map.hpp
@@ -6,8 +6,8 @@ namespace ft
 {
     template < class Key,
                class T,
-               class Compare = ft::less<Key>
-               //class Alloc = std::allocator<pair<const key, T> >
+               class Compare = ft::less<Key>,
+               class Alloc = std::allocator<ft::pair<const Key, T> >
              >
     class map
     {
