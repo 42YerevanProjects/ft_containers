bacd166ff6b3e238976ce4d8bfd6b7da8aa83fdd
Sargis-Hovsepyan <sargishovsepyan2001@gmail.com>
2022-08-04 16:16:59 +0400

    Got rid of the base class

--- Utility/rb_tree_node.hpp
+++ Utility/rb_tree_node.hpp
@@ -3,82 +3,26 @@
 namespace ft
 {
     /*
-    ==========================
-        Rb Node Base Class
-    ==========================
+    =====================
+        RB Node Class
+    =====================
     */
 
     enum rb_tree_color { Red = false, Black = true };
-
-    struct rb_tree_node_base
-    {
-        typedef rb_tree_node_base*          base_ptr;
-        typedef const rb_tree_node_base*    const_base_ptr;
-
-        rb_tree_color   color;
-        base_ptr        parent;
-        base_ptr        left;
-        base_ptr        right;
-
-
-        rb_tree_node_base() : left(), right(), parent(), color(Black) {}
-        rb_tree_node_base(const rb_tree_node_base& other) { *this = other; }
-
-        rb_tree_node_base&  operator=(const rb_tree_node_base& other)
-        {
-            this->left = other.left;
-            this->right = other.right;
-            this->parent = other.parent;
-            this->color = other.color;
-
-            return (*this);
-
-        }
-
-        static base_ptr            minimum(base_ptr x)
-        {
-            while (x->left != 0)
-                x = x->left;
-            return x;
-        }
-
-        static const_base_ptr      minimum(const_base_ptr x)
-        {
-            while (x->left != 0)
-                x = x->left;
-            return x;
-        }
-
-        static base_ptr            maximum(base_ptr x)
-        {
-            while (x->right != 0)
-                x = x->right;
-            return x;
-        }
-
-        static const_base_ptr      maximum(const_base_ptr x)
-        {
-            while (x->right != 0)
-                x = x->right;
-            return x;
-        }
-    };
-
-    /*
-    =====================
-        Rb Node Class
-    =====================
-    */
     
     template<typename T>
-    struct rb_tree_node : rb_tree_node_base
+    struct rb_tree_node
     {
         typedef rb_tree_node<T>*    link_type;
 
-        T   _data;
+        rb_tree_color   color;
+        link_type       parent;
+        link_type       left;
+        link_type       right;
+        T               data;
 
-        rb_tree_node() : rb_tree_node_base() {}
-        rb_tree_node(const rb_tree_node& other) : rb_tree_node_base(other) { _data = other._data; }
+        rb_tree_node() : color(Black), parent(), left(), right() {}
+        rb_tree_node(const rb_tree_node& other) { *this = other; }
 
         rb_tree_node&   operator=(const rb_tree_node& other)
         {
