6ea9bf48df74222980937771676af38bb3ff6487
Sargis-Hovsepyan <sargishovsepyan2001@gmail.com>
2022-08-19 14:50:06 +0400

    [UPDATE] Rearranged

--- Containers/map.hpp
+++ Containers/map.hpp
@@ -5,10 +5,11 @@
 
 namespace ft
 {
-    template < class Key,
-               class T,
-               class Compare = ft::less<Key>,
-               class Alloc = std::allocator<ft::pair<const Key, T> >
+    template < 
+              class Key,
+              class T,
+              class Compare = ft::less<Key>,
+              class Alloc = std::allocator<ft::pair<const Key, T> >
              >
     class map
     {
