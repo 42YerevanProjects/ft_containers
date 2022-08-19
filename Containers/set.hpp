802f87ccd0258ee640582c357cc388c4a594774a
Sargis-Hovsepyan <sargishovsepyan2001@gmail.com>
2022-08-19 15:21:03 +0400

    [ADD] Added the member type definitions

--- Containers/set.hpp
+++ Containers/set.hpp
@@ -13,15 +13,27 @@ namespace ft
     class set
     {
         public:
-            typedef Key                 key_type;
-            typedef Key                 value_type;
-            typedef Compare             key_compare;
-            typedef Compare             value_compare;
-            typedef Alloc               allocator_type;
+            typedef Key                                         key_type;
+            typedef Key                                         value_type;
+            typedef Compare                                     key_compare;
+            typedef Compare                                     value_compare;
+            typedef Alloc                                       allocator_type;
 
         private:
             typedef typename Alloc::template rebind<key_type>::other                                        key_allocator;
-            typedef ft::rb_tree<ket_type, value_type, select_self<value_type>, key_compare, key_allocator>  rb_tree;
+            typedef ft::rb_tree<key_type, value_type, select_self<value_type>, key_compare, key_allocator>  rb_tree;
 
+        public:
+            typedef typename key_allocator::reference           reference;               
+            typedef typename key_allocator::const_reference     const_reference;               
+            typedef typename key_allocator::pointer             pointer;               
+            typedef typename key_allocator::const_pointer       const_pointer;               
+            typedef ptrdiff_t                                   difference_type;
+            typedef size_t                                      size_type;
+
+            typedef typename rb_tree::iterator                  iterator;
+            typedef typename rb_tree::const_iterator            const_iterator;
+            typedef typename rb_tree::reverse_iterator          reverse_iterator;
+            typedef typename rb_tree::const_reverse_iterator    const_reverse_iterator;
     };
 }
