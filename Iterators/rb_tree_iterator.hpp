8f30cf85c5c21828778a44801b90c58c8fdb7c27
Sargis-Hovsepyan <sargishovsepyan2001@gmail.com>
2022-08-22 22:32:32 +0400

    [UPDATE] Put the code of the const_rb_tree here

--- Iterators/rb_tree_iterator.hpp
+++ Iterators/rb_tree_iterator.hpp
@@ -3,15 +3,18 @@
 #include "iterator.hpp"
 #include "../Utility/rb_tree_node.hpp"
 
+
 /*
 ===============================
     Red-Black Tree Iterator
 ===============================
 */
 
-
 namespace ft
 {
+    template <typename T>
+    class	const_rb_tree_iterator;
+
     template <typename T>
     class rb_tree_iterator : ft::iterator<ft::bidirectional_iterator_tag, T>
     {
@@ -21,6 +24,18 @@ namespace ft
 
             template <typename U>
 			friend class const_rb_tree_iterator;
+
+            template <typename V>
+            friend bool	operator==(const rb_tree_iterator<V>& lhs, const rb_tree_iterator<V>& rhs);
+            template <typename V>
+			friend bool operator==(const rb_tree_iterator<V>& lhs, const const_rb_tree_iterator<V>& rhs);
+			template <typename V>
+			friend bool operator==(const const_rb_tree_iterator<V>& lhs, const rb_tree_iterator<V>& rhs);
+
+			template <typename U, typename V>
+			friend bool operator==(const rb_tree_iterator<U>& lhs, const const_rb_tree_iterator<V>& rhs);
+			template <typename U, typename V>
+			friend bool operator==(const const_rb_tree_iterator<U>& lhs, const rb_tree_iterator<V>& rhs);
 
         public:
             typedef typename ft::iterator<ft::bidirectional_iterator_tag, T>::value_type        value_type;
@@ -61,15 +76,83 @@ namespace ft
 
             pointer     operator->() const;
             reference   operator*() const;
-
-            /* Equality Checks */
-
-            bool        operator==(const self& x) const;
-            bool        operator!=(const self& x) const;
-
-            /* Base Node */
-            const base_ptr    base_node() const;
     };
 }
 
 #include "rb_tree_iterator.tpp"
+
+
+/*
+=====================================
+    Red-Black Tree Const Iterator
+=====================================
+*/
+
+namespace ft
+{
+    template <typename T>
+    class const_rb_tree_iterator : ft::iterator<ft::bidirectional_iterator_tag, const T>
+    {
+        private:
+            template <class K, class V, class KOV, class C, class A>
+			friend class rb_tree;
+
+            template <typename V>
+            friend bool operator==(const const_rb_tree_iterator<V>& lhs, const const_rb_tree_iterator<V>& rhs);
+			template <typename V>
+			friend bool operator==(const rb_tree_iterator<V>& lhs, const const_rb_tree_iterator<V>& rhs);
+			template <typename V>
+			friend bool operator==(const const_rb_tree_iterator<V>& lhs, const rb_tree_iterator<V>& rhs);
+
+			template <typename U, typename V>
+			friend bool operator==(const rb_tree_iterator<U>& lhs, const const_rb_tree_iterator<V>& rhs);
+			template <typename U, typename V>
+			friend bool operator==(const const_rb_tree_iterator<U>& lhs, const rb_tree_iterator<V>& rhs);
+
+        public:
+            typedef typename ft::iterator<ft::bidirectional_iterator_tag, const T>::value_type        value_type;
+            typedef typename ft::iterator<ft::bidirectional_iterator_tag, const T>::pointer           pointer;
+            typedef typename ft::iterator<ft::bidirectional_iterator_tag, const T>::reference         reference;
+            typedef typename ft::iterator<ft::bidirectional_iterator_tag, const T>::difference_type   difference_type;
+            typedef typename ft::iterator<ft::bidirectional_iterator_tag, const T>::iterator_category iterator_category;
+
+            typedef rb_tree_iterator<T>         iterator;
+
+            typedef const_rb_tree_iterator<T>   self;
+            typedef ft::rb_tree_node_base*      base_ptr;
+            typedef const rb_tree_node<T>*      link_type;
+
+        private:
+            base_ptr    inorder_increment(base_ptr node);
+            base_ptr    inorder_decrement(base_ptr node);
+            bool        is_sentinel(base_ptr node);
+
+        protected:
+            base_ptr    _node;
+
+        public:
+            /* Constructors and Destructor */
+
+            const_rb_tree_iterator();
+            explicit const_rb_tree_iterator(base_ptr x);
+            const_rb_tree_iterator(const iterator& _it);
+            const_rb_tree_iterator(const self& other);
+            virtual ~const_rb_tree_iterator();
+
+            /* Operator Overloads */
+
+            self&       operator=(const self& other);
+
+            self&       operator++();
+            self        operator++(int);
+
+            self&       operator--();
+            self        operator--(int);
+
+            pointer     operator->() const;
+            reference   operator*() const;
+    };
+}
+
+#include "const_rb_tree_iterator.tpp"
+
